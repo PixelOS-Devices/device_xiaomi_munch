fb9f459b252ac8f6355620d42a9d1bb8fa468e34
TheScarastic <warabhishek@gmail.com>
2018-12-28 15:13:24 -0500

    munch: parts: Add DiracSound backend
    Special thanks to vasishath for his base work on mi3
    
    Change-Id: Id81e60a18dc34c991b82a5a2685c9041222a6ea7

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -22,6 +22,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
