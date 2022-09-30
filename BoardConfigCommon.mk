4a742205bef5695784681d5a9191e2cc8bf2c5e9
itsurboimasarou <medicspam6@gmail.com>
2022-09-30 15:10:34 +0700

    sm8250-common: Define source vendor framework device HALs
    * In addition to Lineage hardware interfaces HALs compatibility
    
    Signed-off-by: itsurboimasarou <medicspam6@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -90,7 +90,9 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/xiaomi:libudfps_extension.xiaomi
 endif
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/framework_compatibility_matrix.xml \
+    vendor/origami/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 ODM_MANIFEST_SKUS += nfc
