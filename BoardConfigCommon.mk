cc29b6c3e080029adebdd58f8a169b224b1ceb88
itsurboimasarou <medicspam6@gmail.com>
2022-10-21 08:32:55 +0700

    Revert "sm8250-common: Move to EROFS"
    This reverts commit ba6a41dfe937b9dd6b6ad6a61dfdade9d1435c6d.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -141,7 +141,7 @@ TREBLE_PARTITIONS := odm vendor
 ALL_PARTITIONS := $(SSI_PARTITIONS) $(TREBLE_PARTITIONS)
 
 $(foreach p, $(call to-upper, $(ALL_PARTITIONS)), \
-    $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := erofs) \
+    $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := ext4) \
     $(eval TARGET_COPY_OUT_$(p) := $(call to-lower, $(p))))
 
 # Partitions - dynamic
