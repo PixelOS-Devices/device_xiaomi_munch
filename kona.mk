76cd3e4b1e3421ecb5ae0d0f703b981dad339e02
itsurboimasarou <medicspam6@gmail.com>
2022-10-25 21:33:51 +0700

    Revert "[DNM] sm8250-common: Don't build source vendor.qti.hardware.camera.device@1.0"
    * Currently just ship the source built one because we aren't stabilize MIUI Camera yet (and the device inbuilt blobs are there)
    
    This reverts commit 1052f1150a1f0b4c646c2ce63dc893e975984065.

--- kona.mk
+++ kona.mk
@@ -160,6 +160,7 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libgui_vendor \
     libstdc++.vendor \
+    vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 # Configstore
