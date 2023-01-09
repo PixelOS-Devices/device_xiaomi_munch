4f1ea0e9a8d2de540d021d0e9651c9a13052f774
itsurboimasarou <medicspam6@gmail.com>
2023-01-09 09:04:02 +0700

    munch: Inherit common_full_phone.mk
    * Sync with latest PixelOS changes
    
    Signed-off-by: itsurboimasarou <medicspam6@gmail.com>

--- aosp_munch.mk
+++ aosp_munch.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from munch device
 $(call inherit-product, device/xiaomi/munch/device.mk)
