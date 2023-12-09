f648e2c391daa0c672a91cdca775614e56f6bc38
Michael Bestas <mkbestas@lineageos.org>
2023-12-09 20:00:07 +0200

    munch: Don't build libtinyxml
    Not used anywhere
    
    Change-Id: I95d2296321b8f597fbf12bd3f57b4b2f512008f1

--- device.mk
+++ device.mk
@@ -209,7 +209,6 @@ PRODUCT_PACKAGES += \
     libqdMetaData.system \
     libsdmcore \
     libsdmutils \
-    libtinyxml \
     libvulkan \
     vendor.display.config@1.5 \
     vendor.display.config@1.11.vendor \
